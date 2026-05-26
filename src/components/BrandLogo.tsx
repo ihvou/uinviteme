b1a323088d36203df8d998871b98de54495a25d3
ihvou <serhii.knyr@gmail.com>
2026-05-27 00:24:27 +0800

    Use transparent vector logo

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -9,9 +9,9 @@ interface BrandLogoProps {
 }
 
 const markSizes = {
-  sm: "h-6 w-6",
-  md: "h-8 w-8",
-  lg: "h-10 w-10",
+  sm: "h-8 w-8",
+  md: "h-10 w-10",
+  lg: "h-12 w-12",
 };
 
 const textSizes = {
